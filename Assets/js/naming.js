b880531ed3beaceb84a842659e7c09fae7344103
RiccardoSennati <111577140+RiccardoSennati@users.noreply.github.com>
2022-10-10 15:46:18 +0200

    Add files via upload

---
+++ Assets/js/naming.js
@@ -0,0 +1,16 @@
+var  pet_preferito = 'gatto';
+console.log( pet_preferito);
+
+var pet = 'gatto';
+var Pet = 'cane';
+var petPreferito = 'criceto';
+var PetPreferito = 'coniglio';
+
+var $pet = 'giraffa';
+console.log($pet);
+var _pet = 'leone';
+console.log(_pet);
+var _pet2 = 'pantera';
+document.write(_pet2);
+
+var numero12 = '23';
